1338f1e6dabf358e336013879ab55f6da5f2542a
nikitoshom <nikitos_hom@mail.ru>
2017-06-28 21:10:49 +0300

    Update Dev1.java

--- Dev1.java
+++ Dev1.java
@@ -1,8 +1,8 @@
 public class Dev1 {
-  public static void main(String[] args) {
-    for(int i = 0; i <= 100; i++) {
-      if(i % 3 == 0) {
-        System.out.println("3 * " + i/3);
+  public static void main (String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      if (i % 3 == 0) {
+        System.out.println("3*" + i/3);
       } else {
 	  System.out.println(i);			
 	}
