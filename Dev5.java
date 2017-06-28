5b422b9e9f8eced03b94ca183cca203a88a9fc74
nikitoshom <nikitos_hom@mail.ru>
2017-06-29 00:14:25 +0300

    Add files via upload

---
+++ Dev5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Dev5 {
+  public static void main (String[] args) {
+    if (args.length == 0){
+      System.out.println("Please enter sequence of numbers and use space for separation:");
+      Scanner numbers = new Scanner(System.in);
+      String str = numbers.nextLine();
+      String chisla[] = str.split(" "); 
+      Test(chisla);
+    } else {
+        Test(args);
+      }
+  }
+/*
+ *This method checks the sequence
+ */
+  public static void Test (String [] chisla){
+    int n = 1;
+    for (int i = 1; i < chisla.length; i++) {
+      if(Integer.parseInt(chisla[i]) < Integer.parseInt(chisla[i-1])){ 
+        n++;
+      }
+    }
+    if (n == chisla.length) {
+      System.out.println("ubivauzhay");
+    } else {
+        System.out.println("neubivauzhay");
+      }
+  }
+}
