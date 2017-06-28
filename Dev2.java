e048822045b8ccebf2e02b3bcfba2a7788aef8f9
nikitoshom <nikitos_hom@mail.ru>
2017-06-28 13:22:54 +0300

    Add files via upload

---
+++ Dev2.java
@@ -0,0 +1,25 @@
+public class Dev2 {
+	public static void main(String[] args) {
+		int N = 100;
+		for(int i = 0; i <= 100; i++) {
+			if(i % 3 == 0 && i % 5 == 0) {
+				System.out.println("Tutti-Frutti");
+			}
+			else {
+				if(i % 3 == 0) {
+					System.out.println("Tutti");
+				}
+				else {
+					if(i % 5 == 0) {
+						System.out.println("Frutti");
+					}
+					else {
+						System.out.println(i);			
+					}
+				}
+
+			}
+		}
+		
+	}
+}
